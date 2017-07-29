db19d54dcdac77d7f3633516a7778a48c97e6792
Zoli Saunt <zoli@Zolis-MacBook-Pro.local>
2017-07-29 03:15:29 -0400

    Psychic-Game

--- assets/js/psychicGame.js
+++ assets/js/psychicGame.js
@@ -1,55 +1,77 @@
-   var compChoices = "bcdefghijklmnopqrstuvwxyz";
-   var wins = 0;
-   var losses = 0;
-   var triesRemaining = 10;
-   var compGuess = null;
-   //An empty array to start for userguesses
-   var userGuesses = [];
+  var compChoices = "abcdefghijklmnopqrstuvwxyz";
+  var wins = 0;
+  var losses = 0;
+  var triesRemaining = 10;
+  var compGuess = compChoices.charAt(Math.floor(Math.random() * compChoices.length));
+  //An empty array to start for userguesses
+  var userGuesses = [];
 
-   //This def is icky.  Gotta be abetter way
-   var winsEl = document.getElementById('wins');
-   var lossesEl = document.getElementById('losses');
-   //var userGuessEl = document.getElementById('userGuess');
-   var usGuessesEl = document.getElementById('userGuesses');
-   var triesRemEl = document.getElementById('triesRemaining');
-
-   var reset = function() {
-     triesRemaining = 10;
-     userGuesses = [];
-     compGuess = compChoices.charAt(Math.floor(Math.random() * compChoices.length));
-   };
+  //This def is icky.  Gotta be abetter way... 
+  var winsEl = document.getElementById('wins');
+  var lossesEl = document.getElementById('losses');
+  var userGuessEl = document.getElementById('userGuess');
+  var usGuessesEl = document.getElementById('userGuesses');
+  var triesRemEl = document.getElementById('triesRemaining');
   
+  var reset = function() {
+    triesRemaining = 10;
+    userGuesses = [];
+    compGuess = compChoices.charAt(Math.floor(Math.random() * compChoices.length));
+  };
+
+  var runGame = function(userGuess, compGuess) {
+    console.log(compGuess);
+    while (triesRemaining > 0 && userGuess !== compGuess) {
+      userGuesses = userGuesses + " " + userGuess;
+      return triesRemaining--;
+    }
+
+    if (triesRemaining === 0) {
+      alert ("Sorry you lost");
+      reset();
+      return losses++;
+    }
+
+    else {
+      alert("You Won: The Letter was " + compGuess);
+      reset();
+      return wins++; 
+    }
+  };
+
+  /*Another possible way to run the game with nested if statements.  
+
+  var runGame = function(userGuess, compGuess) {  
+    console.log(compGuess); 
+    if (triesRemaining > 0) {
+
+      if (userGuess === compGuess) {
+        alert("You Won: The Letter was " + compGuess);
+        reset();
+        return wins++; 
+      }
+
+      else {
+        userGuesses = userGuesses + " " + userGuess;
+        return triesRemaining--;
+      }
+    }
+
+    else {
+      alert("Sorry you lost");
+      reset();
+      return losses++;
+    }
+  };
+*/
+  
+  //When you hit a key, it runs the game and ties in the variables with html elements
+  document.onkeyup = function(event) {
+    var userGuess = event.key;
+    runGame(userGuess, compGuess);
+    usGuessesEl.textContent = userGuesses;
+    winsEl.textContent = wins;
+    lossesEl.textContent = losses;
+    triesRemEl.textContent = triesRemaining;
+  };
    
-
-
-   var runGame = function(choice, compGuess) {
-     
-     if (triesRemaining > 0) {
-
-       if (choice === compGuess) {
-         alert("You Won: The Letter was " + compGuess);
-         reset();
-         return wins++; 
-       }
-
-       else {
-         return triesRemaining--;
-       }
-     }
-
-     else {
-       alert("Sorry you lost");
-       reset();
-       return losses++;
-     }
-
-   };
-
-   document.onkeyup = function(event) {
-     var userGuess = event.key;
-     runGame(userGuess, compGuess);
-     usGuessesEl.textContent = userGuesses;
-     winsEl.textContent = wins;
-     lossesEl.textContent = losses;
-     triesRemEl.textContent = triesRemaining;
-   };
